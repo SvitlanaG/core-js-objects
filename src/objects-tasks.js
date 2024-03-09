1404034ebe011855667209464bfb47085979b623
Svitlana <svitlana.grytsai@gmail.com>
2024-03-09 18:33:14 +0100

    feat: implement two more function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -191,8 +191,15 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectangle = {
+    width,
+    height,
+    getArea() {
+      return width * height;
+    },
+  };
+  return rectangle;
 }
 
 /**
@@ -205,8 +212,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
