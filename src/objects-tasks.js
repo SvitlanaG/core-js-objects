66488dd34ac3c8123ef5cef1f9d6cb90d1a5dfae
Svitlana <svitlana.grytsai@gmail.com>
2024-03-15 22:37:33 +0100

    feat: implement makeWord function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -129,8 +129,22 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function findLetter(obj, value) {
+  let letter = '-';
+  Object.keys(obj).forEach((key) => {
+    if (obj[key].includes(value)) {
+      letter = key;
+    }
+  });
+  return letter;
+}
+function makeWord(lettersObject) {
+  let word = '';
+  const length = Math.max(...Object.values(lettersObject).flat());
+  for (let i = 0; i <= length; i += 1) {
+    word += findLetter(lettersObject, i);
+  }
+  return word;
 }
 
 /**
